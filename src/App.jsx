61eb7ee231fe62099d767bff0c4b615e4c7590d1
abolfazl <abolfazlkhazaei999@gmail.com>
2023-11-26 20:46:31 +0330

    main grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import LeftColumn from './components/LeftColumn'
 import Navbar from './components/Navbar'
+import RightColumn from './components/RightColumn'
 import SideBar from './components/SideBar'
 
 function App() {
@@ -9,8 +11,16 @@ function App() {
       <SideBar />
      <div className='flex flex-col flex-1 relative'>
      <Navbar />
+     
+     <div className='grid md:grid-cols-3 grid-cols-1 w-full'>
+      <div className='col-span-2'>
+        <LeftColumn />
+      </div>
+      <div w-full>
+        <RightColumn />
+      </div>
+     </div> 
      </div>
- 
 
     </main>
   
---
+++ src/components/LeftColumn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftColumn = () => {
+    return (
+        <div className='w-full flex flex-col justify-between p-2'>
+            <div className='flex flex-col lg:flex-row gap-2 w-full'></div>
+        </div>
+    );
+};
+
+export default LeftColumn;
