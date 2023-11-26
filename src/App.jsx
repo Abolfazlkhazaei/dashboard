b828d589c541bc5d8fc92b6c5ca1db4cf342a68e
abolfazl <abolfazlkhazaei999@gmail.com>
2023-11-26 19:51:20 +0330

    text input in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-
-
+import Navbar from './components/Navbar'
 import SideBar from './components/SideBar'
 
 function App() {
@@ -8,6 +7,10 @@ function App() {
   return (
     <main className='flex'>
       <SideBar />
+     <div className='flex flex-col flex-1 relative'>
+     <Navbar />
+     </div>
+ 
 
     </main>
   
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { TextInput } from '@tremor/react';
+import {SearchIcon} from "@heroicons/react/solid"
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div id='top' className='relative w-full justify-between items-center p-2'>
+            <h1 className='font-bold text-gray-300 '>Dashboard</h1>
+            <div className='py-2 '>
+              <TextInput placeholder='Search....' icon={SearchIcon} />
+            </div>
+            
+        </div>
+    );
+};
+
+export default Navbar;
