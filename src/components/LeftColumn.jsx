f223c435724798ecad7d43c0f30c33fcbe2a22d7
abolfazl <abolfazlkhazaei999@gmail.com>
2023-11-27 00:14:52 +0330

    AreaChart in leftColumn

--- src/components/LeftColumn.jsx
+++ src/components/LeftColumn.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardItem from './CardItem';
+import AreaChartComponent from './AreaChartComponent';
 
 const LeftColumn = () => {
     return (
@@ -9,6 +10,9 @@ const LeftColumn = () => {
                 <CardItem />
                 <CardItem />
                 <CardItem />
+            </div>
+            <div className='flex-auto w-full'>
+                <AreaChartComponent />
             </div>
         </div>
     );
