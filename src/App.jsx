571301fe876a0b64246c124cc41a68574f861f3e
abolfazl <abolfazlkhazaei999@gmail.com>
2023-11-27 01:08:13 +0330

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
-import LeftColumn from './components/LeftColumn'
-import Navbar from './components/Navbar'
-import RightColumn from './components/RightColumn'
-import SideBar from './components/SideBar'
+import LeftColumn from "./components/LeftColumn";
+import Navbar from "./components/Navbar";
+import RightColumn from "./components/RightColumn";
+import Sidebar from "./components/Sidebar";
 
 function App() {
- 
-
   return (
-    <main className='flex'>
-      <SideBar />
-     <div className='flex flex-col flex-1 relative'>
-     <Navbar />
-     
-     <div className='grid md:grid-cols-3 grid-cols-1 w-full'>
-      <div className='col-span-2'>
-        <LeftColumn />
+    <main className="flex">
+      <Sidebar />
+      <div className="flex flex-col flex-1 relative">
+        <Navbar />
+        <div className="grid md:grid-cols-3 grid-cols-1 w-full">
+          <div className="col-span-2">
+            <LeftColumn />
+          </div>
+          <div className="w-full">
+            <RightColumn />
+          </div>
+        </div>
       </div>
-      <div w-full>
-        <RightColumn />
-      </div>
-     </div> 
-     </div>
-
     </main>
-  
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,20 +1,16 @@
-import React from 'react';
-import { Card, Metric, Text , BadgeDelta ,Flex } from "@tremor/react";
-
+import React from "react";
+import { BadgeDelta, Card, Flex, Metric, Text } from "@tremor/react";
 
 const CardItem = () => {
-    return (
-        <div>
-            <Card className="w-xs" decoration="top" decorationColor="indigo">
-                <Flex justifyContent='between' alignItems='center'>
-                     <Text className='ml-2'>Sales</Text>
-                    <BadgeDelta deltaType="moderateIncrease">+12.5%</BadgeDelta>
-                </Flex>
-                <Metric>$ 34,743</Metric>
-            </Card>
-            
-        </div>
-    );
+  return (
+    <Card className="w-xs" decoration="top" decorationColor="indigo">
+      <Flex justifyContent="between" alignItems="center">
+        <Text>Sales</Text>
+        <BadgeDelta deltaType="moderateIncrease">+12.5%</BadgeDelta>
+      </Flex>
+      <Metric>$ 34,743</Metric>
+    </Card>
+  );
 };
 
 export default CardItem;
