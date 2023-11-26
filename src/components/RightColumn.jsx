a0ad7461cb57434898e10726b73c7bb2a86f0805
abolfazl <abolfazlkhazaei999@gmail.com>
2023-11-27 01:00:36 +0330

    Sales item in Right Column

--- src/components/RightColumn.jsx
+++ src/components/RightColumn.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import SalesItem from "./SalesItem";
+import WebAnalytics from "./WebAnalytics";
+import ScoreList from "./ScoreList";
 
 const RightColumn = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="w-full p-2">
+      <SalesItem />
+      <WebAnalytics />
+      <ScoreList />
+    </div>
+  );
 };
 
 export default RightColumn;
